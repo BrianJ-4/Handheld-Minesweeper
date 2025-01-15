263bb88b4dd56d5171dd5d244f2a08c4a2c3a95e
BrianJ-4 <brian.joshua03@gmail.com>
2025-01-14 17:26:13 -0800

    Add game start

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include "LEDController.h"
+#include "GameController.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define ROWS 10
+#define COLS 6
+
+const char keys[ROWS][COLS] = {
+  {'0', '1', '2', '3', '4', '5'},
+  {'6', '7', '8', '9', 'A', 'B'},
+  {'C', 'D', 'E', 'F', 'G', 'H'},
+  {'I', 'J', 'K', 'L', 'M', 'N'},
+  {'O', 'P', 'Q', 'R', 'S', 'T'},
+  {'U', 'V', 'W', 'X', 'Y', 'Z'},
+  {'a', 'b', 'c', 'd', 'e', 'f'},
+  {'g', 'h', 'i', 'j', 'k', 'l'},
+  {'m', 'n', 'o', 'p', 'q', 'r'},
+  {'s', 't', 'u', 'v', 'w', 'x'}
+};
+
+uint8_t rowPins[ROWS] = {16, 4, 2, 13, 32, 33, 25, 26, 27, 14};
+uint8_t colPins[COLS] = {22, 5, 17, 21, 19, 18};
+
+LEDController *ledController;
+Adafruit_Keypad *keypad;
+GameController *game;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  ledController = new LEDController();
+  keypad = new Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+  keypad->begin();
+  game = new GameController(*ledController, *keypad);
+  game->startGame();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int row, col;
+  bool shortPress;
+  game->getInput(row, col, shortPress);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Row: " + String(row) + " Col: " + String(col) + " Short?: " + String(shortPress));
 }
